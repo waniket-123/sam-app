fbf65170c0d08fa1921d42ec5f2aab04c93801cf
Aniketh <waniket@example.com>
2025-02-20 19:05:38 +0000

    Changed return message

--- hello_world/app.py
+++ hello_world/app.py
@@ -34,9 +34,8 @@ def lambda_handler(event, context):
     #     raise e
 
     return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello my friend",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+    "statusCode": 200,
+    "body": json.dumps({
+        "message": "I'm using canary deployments",
+    }),
+}
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -69,4 +69,4 @@ def test_lambda_handler(apigw_event):
 
     assert ret["statusCode"] == 200
     assert "message" in ret["body"]
-    assert data["message"] == "hello my friend"
+    assert data["message"] == "I'm using canary deployments"
